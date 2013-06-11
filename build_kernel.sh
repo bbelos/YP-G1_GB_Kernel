7659f5cc006e65c3e945330420cb318b1ab77156
Brian Beloshapka <bbelos@hotmail.com>
2013-06-11 11:39:37 -0400

    Switch defconfig in build script.

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,7 @@ export CROSS_COMPILE=/home/bbelos/toolchains/arm-2009q3/bin/arm-none-linux-gnuea
 INITRAMFS_SOURCE=../epic3buttonfix
 INITRAMFS_DIR="usr/initramfs"
 
-make palladio_rev01_defconfig
+make palladio_recovery.bin_defconfig
 export LOCALVERSION="-G1XXKPN-CL562447"
 rm $INITRAMFS_DIR/* -r
 cp $INITRAMFS_SOURCE/* $INITRAMFS_DIR/ -r
